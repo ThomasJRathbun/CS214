99fd0fd199567b4b67d2909c6addbd987456d891
Thomas Rathbun <thomasjrathbun@gmail.com>
2017-09-28 22:59:41 -0400

    prototyping mergesort

---
+++ mergesort.c
@@ -0,0 +1,17 @@
+#include "mergesort.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct _node
+{
+  char ** date;
+  struct * _node;
+}node;
+
+
+void mergesort( node * head, int left, int right, int (*comp)(void *, void *))
+{
+
+
+}
---
+++ mergesort.h
@@ -0,0 +1,10 @@
+#ifndef MERGESORT_H
+#define MERGESORT_H
+
+void mergeSort( node * head, int left, int right, 
+
+
+
+
+
+#endif
